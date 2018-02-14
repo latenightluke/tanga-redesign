ec2e7f9650f813724aec81ecb6798e5dda05f652
Luke Ivie <luke.ivie@gmail.com>
2018-02-14 15:56:51 -0700

    Updated favicon, cleared README

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom'
 import { HashRouter } from 'react-router-dom';
 
-import registerServiceWorker from './registerServiceWorker';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import styles from './styles/styles.scss';
 
 class TangaLibrary extends Component {
