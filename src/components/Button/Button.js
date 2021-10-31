57ba93ae31162bf36e7a9b565650630fba55cb52
latenightluke <luke.ivie@icloud.com>
2021-10-31 10:35:20 -0700

    Feat: change font and style fixes

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
-import classNames from 'classnames';
-import styles from './Button.module.css';
+import classNames from "classnames";
+import styles from "./Button.module.css";
 let cx = classNames.bind(styles);
 
 class Button extends Component {
@@ -13,28 +13,40 @@ class Button extends Component {
       styles[this.props.theme],
       styles[this.props.size],
       styles[this.props.border]
-    )
+    );
 
     return (
-      <NavLink to={this.props.to} activeClassName="button-active" className={buttonClasses + " " + this.props.className} style={{width: `${this.props.width}`}}>
+      <NavLink
+        to={this.props.to}
+        activeClassName="button-active"
+        className={buttonClasses + " " + this.props.className}
+        style={{ width: `${this.props.width}` }}
+      >
         {this.props.children}
       </NavLink>
-    )
+    );
   }
 }
 
 Button.propTypes = {
-  theme: PropTypes.oneOf(['primary', 'secondary', 'danger', 'success', 'info', 'warning']),
+  theme: PropTypes.oneOf([
+    "primary",
+    "secondary",
+    "danger",
+    "success",
+    "info",
+    "warning",
+  ]),
   width: PropTypes.string,
-  size: PropTypes.oneOf(['sm', 'md', 'lg']),
-  to: PropTypes.string
+  size: PropTypes.oneOf(["sm", "md", "lg"]),
+  to: PropTypes.string,
 };
 
 Button.defaultProps = {
   theme: "primary",
   width: "",
   size: "md",
-  to: ""
+  to: "",
 };
 
 export default Button;
--- src/components/NewsBanner/NewsBanner.js
+++ src/components/NewsBanner/NewsBanner.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Container } from 'reactstrap';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { Container } from "reactstrap";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 
-import styles from './NewsBanner.module.css';
+import styles from "./NewsBanner.module.css";
 
 class NewsBanner extends Component {
   render() {
     return (
-      <Container fluid className={styles.base + " bg-dark-red text-white text-center text-md-left fs-xs px-md-4 px-2 d-flex justify-content-between"}>
+      <Container
+        fluid
+        className={
+          styles.base +
+          " bg-dark-red text-white text-center text-md-left fs-xs px-md-4 px-2 d-flex justify-content-between"
+        }
+      >
         <div className="d-inline-flex fw-regular text-center text-md-left align-self-center">
-          <span>Free Returns</span><span className="mx-2">•</span>100% Risk Free, 100% Easy <FontAwesomeIcon icon="long-arrow-alt-right" className="ml-2 mt-1" />
+          <span>Free Returns</span>
+          <span className="mx-2">•</span>100% Risk Free, 100% Easy{" "}
+          <FontAwesomeIcon icon="long-arrow-alt-right" className="ml-2 mt-1" />
         </div>
-        <NavLink to="/" className="d-none d-md-flex text-white">
+        {/* <NavLink to="/" className="d-none d-md-flex text-white">
           <FontAwesomeIcon icon="long-arrow-alt-left" className="mr-2 mt-1"/>Back To Pattern Library
-        </NavLink>
+        </NavLink> */}
         <div className="fw-regular d-none d-md-flex">
           <NavLink to="/" className="NavLink text-white mr-4">
             <span className="text">
-              <FontAwesomeIcon icon="heart" className="mr-2 mb-1" />My Likes</span>
+              <FontAwesomeIcon icon="heart" className="mr-2" />
+              My Likes
+            </span>
           </NavLink>
           <NavLink to="/" className={styles.link + " text-white"}>
             <span className="text">
-              <FontAwesomeIcon icon="truck" className="mr-2 mb-1" />Track An Order
+              <FontAwesomeIcon icon="truck" className="mr-2" />
+              Track An Order
             </span>
           </NavLink>
         </div>
